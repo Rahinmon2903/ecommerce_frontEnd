86e8f456dfc25a0b1e7b8404c275d4966735b65e
Rahinmon2903 <rahin2903@gmail.com>
2025-12-31 13:45:40 +0530

    productdetials and seller dashbord completed

--- src/Pages/ProductDetials.jsx
+++ src/Pages/ProductDetials.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../Services/api';
 
 const ProductDetials = () => {
+    const {id} = useParams();
+    const[product, setProduct] = useState(null);
 
+    useEffect(()=>{
+        fetchingDetials();
+    },[id])
+
+    const fetchingDetials=async()=>{
+        try {
+             const response=await api.get(`/products/getById/${id}`);
+        setProduct(response.data.product)
+            
+        } catch (error) {
+              console.error("Error fetching product:", error);
+      setProduct(null);
+            
+        }
+       
+    }
     
+      if (!product) {
+    return <p className="p-6">no product Found</p>;
+  }
+
     return (
         <div>
+              <div className="p-6">
+      <h1 className="text-2xl font-bold">{product.name}</h1>
+      <p className="mt-2 text-gray-600">{product.description}</p>
+      <p className="mt-2 font-semibold">₹ {product.price}</p>
+      <p className="text-sm text-gray-500">
+        Seller: {product.seller?.name}
+      </p>
+    </div>
         
             
         </div>
