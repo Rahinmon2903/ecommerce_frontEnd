f758bf71c31201e5e0c0c7d7e8fd800f2543bff8
Rahinmon2903 <rahin2903@gmail.com>
2025-12-31 18:06:44 +0530

    little updation

--- src/Pages/SellerDashboard.jsx
+++ src/Pages/SellerDashboard.jsx
@@ -11,55 +11,51 @@ const SellerDashboard = () => {
   });
 
   const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit=async(e)=>{
     e.preventDefault();
     try {
-      await api.post("/products/create", {
-        ...form,
-        price: Number(form.price),
-        stock: Number(form.stock),
-      });
-      alert("Product added");
-      setForm({
+        await api.post("/products/create",form);
+        alert("Product added");
+         setForm({
         name: "",
         description: "",
         price: "",
         category: "",
         stock: "",
       });
+        
     } catch (error) {
-      console.error("Error adding product:", error);
-      alert(error.response?.data?.message);
+        console.error("Error adding product:", error);
+          alert(error.response?.data?.message);
+        
     }
-  };
 
-  return (
-    <div>
-      <h1>Seller Dashboard</h1>
-
-      <form onSubmit={handleSubmit}>
+  }
+  return (<div>
+    <h1>Seller Dashboard</h1>
+    <form onSubmit={handleSubmit}>
         <label>Name</label>
-        <input name="name" value={form.name} onChange={handleChange} />
-
+        <input type="text" name="name" value={form.name} onChange={handleChange} />
         <label>Description</label>
-        <input name="description" value={form.description} onChange={handleChange} />
-
+        <input type="text" name="description" value={form.description} onChange={handleChange} />
         <label>Price</label>
-        <input type="number" name="price" value={form.price} onChange={handleChange} />
-
+        <input type="text" name="price" value={form.price} onChange={handleChange} />
         <label>Category</label>
-        <input name="category" value={form.category} onChange={handleChange} />
-
+        <input type="text" name="category" value={form.category} onChange={handleChange} />
         <label>Stock</label>
-        <input type="number" name="stock" value={form.stock} onChange={handleChange} />
-
+        <input type="text" name="stock" value={form.stock} onChange={handleChange} />
         <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+
+    </form>
+
+
+  </div>);
 };
 
 export default SellerDashboard;
