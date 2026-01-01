d1e4b8747fdcc7f1966a39622835d4dd88913763
Rahinmon2903 <rahin2903@gmail.com>
2026-01-01 13:20:29 +0530

    cart page updated

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import api from "../Services/api";
 
 const Cart = () => {
-    return (
-        <div>
-            <h1>Cart</h1>
-            
+  const [cart, setCart] = useState(null);
+
+  useEffect(() => {
+    fetchCart();
+  }, []);
+
+  const fetchCart = async () => {
+    try {
+      const response = await api.get("/cart");
+      setCart(response.data.cart);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (!cart || cart.products.length === 0) {
+    return <p>Your cart is empty</p>;
+  }
+
+  return (
+    <div>
+      <h1>Your Cart</h1>
+
+      {cart.products.map((item) => (
+        <div key={item.productId._id}>
+          <p>{item.productId.name}</p>
+          <p>₹ {item.productId.price}</p>
+          <p>Qty: {item.quantity}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Cart;
